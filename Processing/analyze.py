139cb9dd5a862f5cf336521d8f502cbc6a31d770
ParthShah2007 <parthrshah202@gmail.com>
2026-02-18 18:20:08 +0530

    Created endpoint analysis in analyze.py

--- Processing/analyze.py
+++ Processing/analyze.py
@@ -9,7 +9,7 @@ def load_metrics(filename):
     df=pd.read_csv(filename)
     return df
 
-def analyze_user_metrice(user_metrics):
+def analyze_user_metrics(user_metrics):
     output=[]
     output.append("USER ANALYSIS\n")
     output.append("")#new line in txt file
@@ -59,10 +59,72 @@ def analyze_user_metrice(user_metrics):
     report="\n".join(output)
     with open('Outputs/Reports/user_analysis.txt','w') as f:
         f.write(report)
+        
+def analyze_endpoint_metrics(endpoint_metrics):
+    output=[]
+    output.append("ENDPOINT METRICS\n")
+    output.append("")
     
+    #finding slowest endpoints
+    df1=endpoint_metrics.nlargest(5,'avg_response_time')
+    print("The slowest end-points are :-")
+    print(df1[['endpoint','avg_response_time']].to_string(index=False))
+    print("\n")
+    output.append("The top 5 slowest endpoints are : ")
+    output.append(df1[['endpoint','avg_response_time']].to_string(index=False))
+    output.append("")
+    
+    #finding endpoints with maximum error rates
+    df2=endpoint_metrics.nlargest(5,"error_count")
+    print("The endpoints with maximum error rates are :- ")
+    print(df2[['endpoint','error_count']].to_string(index=False))
+    print("\n")
+    output.append("The top 5 endpoints with maximum error count are :- ")
+    output.append(df2[['endpoint','error_count']].to_string(index=False))
+    output.append("")
+    
+    #finding total traffic distribution across endpoints
+    df3 = endpoint_metrics[['endpoint', 'total_requests']].sort_values('total_requests', ascending=False)
+    print(df3.to_string(index=False))
+    output.append(df3.to_string(index=False))
+    output.append("")
+    
+    #finding endpoint which consumes most bandwidth
+    max_idx=endpoint_metrics['total_bytes'].idxmax()
+    max_bandwidth=endpoint_metrics.loc[max_idx,'total_bytes']
+    max_bw_endpoint=endpoint_metrics.loc[max_idx,'endpoint']
+    print(f"The endpoint which consumes max_bandwidth is {max_bw_endpoint} consuming {max_bandwidth}")
+    print("\n")
+    output.append(f"The endpoint which consumes max_bandwidth is {max_bw_endpoint} consuming {max_bandwidth} bytes")
+    output.append("")
+    
+    #finding percentage of requests which go to API and STATIC files
+    total_requests=endpoint_metrics['total_requests'].sum()
+    
+    total_api_requests=endpoint_metrics.loc[endpoint_metrics['endpoint'].str.contains('/api/', na=False), 'total_requests'].sum()
+    total_static_requests=endpoint_metrics.loc[endpoint_metrics['endpoint'].str.contains('/static', na=False), 'total_requests'].sum()
+    
+    api_percent=((total_api_requests/total_requests)*100).round(2)
+    static_percent=((total_static_requests/total_requests)*100).round(2)
+    
+    print(f"Percentage API requets = {api_percent} %")
+    print(f"Percentage STATIC requets = {static_percent} %")
+    
+    output.append(f"Percentage API requets = {api_percent} %")
+    output.append("")
+    output.append(f"Percentage STATIC requets = {static_percent} %")
+    output.append("")
+    
+    #Joining all lines and writing to file
+    report="\n".join(output)
+    with open('Outputs/Reports/endpoint_analysis.txt','w') as f:
+        f.write(report)
 
 if __name__=="__main__":
     user_metrics=load_metrics("Data/Transformed/user_metrics.csv")
-    analyze_user_metrice(user_metrics)
+    analyze_user_metrics(user_metrics)
+    
+    endpoint_metrics=load_metrics("Data/Transformed/endpoint_metrics.csv")
+    analyze_endpoint_metrics(endpoint_metrics)
     
     
